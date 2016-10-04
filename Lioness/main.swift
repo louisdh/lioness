110fd8d072adfa29511cd832d8c08dea30864553
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-10-04 20:00:05 +0200

    Initial Commit

---
+++ Lioness/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lioness
+//
+//  Created by Louis D'hauwe on 04/10/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
