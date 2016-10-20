0f701613cbe9377a4037d32f4be29ee6daad47af
Louis D'hauwe <louisdhauwe@silverfox.be>
2016-10-20 21:19:40 +0200

    Added performance tests for large math operation

---
+++ Lioness Tests/FullRun+Performance.swift
@@ -0,0 +1,38 @@
+//
+//  FullRun+Performance.swift
+//  Lioness
+//
+//  Created by Louis D'hauwe on 20/10/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+import XCTest
+import Lioness
+
+class FullRun_Performance: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testLargeMathPerformance() {
+		
+		let runner = LionessRunner(logDebug: true)
+
+		let path = "/Users/louisdhauwe/Desktop/Swift/Lioness/Lioness Tests/LargeMathOperation.lion"
+		let source = try! String(contentsOfFile: path, encoding: .utf8)
+		
+        self.measure {
+
+			runner.runSource(source)
+
+		}
+    }
+
+}
---
+++ Lioness Tests/Lexer+Performance.swift
@@ -0,0 +1,39 @@
+//
+//  Lexer+Performance.swift
+//  Lioness
+//
+//  Created by Louis D'hauwe on 20/10/2016.
+//  Copyright © 2016 Silver Fox. All rights reserved.
+//
+
+import XCTest
+import Lioness
+
+class Lexer_Performance: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testLargeMathPerformance() {
+		
+		let path = "/Users/louisdhauwe/Desktop/Swift/Lioness/Lioness Tests/LargeMathOperation.lion"
+		let source = try! String(contentsOfFile: path, encoding: .utf8)
+		
+
+        self.measure {
+
+			let lexer = Lexer(input: source)
+
+			_ = lexer.tokenize()
+
+		}
+    }
+
+}
